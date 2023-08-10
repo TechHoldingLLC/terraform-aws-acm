ac015d64ddd85341e084e6ee38ccf9c92a7ef0f0
niyatir2608 <niyati.ranpara@techholding.co>
2023-08-10 16:34:28 +0530

    Add provider block and modified output.tf file

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "acm_arn" {
 output "acm_domain_validation_options" {
   value = aws_acm_certificate.cert.domain_validation_options
 }
+
+output "acm_domain_name" {
+  value = aws_acm_certificate.cert.domain_name
+}
+
+output "acm_subject_alternative_names" {
+  value = aws_acm_certificate.cert.subject_alternative_names
+}
