f223b6396c0ea6d2ed3eaeb616217ffde2c1bc37
niyatir2608 <niyati.ranpara@techholding.co>
2023-08-10 16:44:18 +0530

    Modify output names

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,10 @@ output "acm_domain_validation_options" {
   value = aws_acm_certificate.cert.domain_validation_options
 }
 
-output "acm_domain_name" {
+output "domain_name" {
   value = aws_acm_certificate.cert.domain_name
 }
 
-output "acm_subject_alternative_names" {
+output "subject_alternative_names" {
   value = aws_acm_certificate.cert.subject_alternative_names
 }
